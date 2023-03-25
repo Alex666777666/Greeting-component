c5ca53b6532d75c1a0cc3459fd44acfa15e40683
Alex <111201814+Alex666777666@users.noreply.github.com>
2023-03-25 18:35:45 +0200

    Update Greeting.jsx

--- src/Greeting.jsx
+++ src/Greeting.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Greeting = props => {
   const age = new Date().getFullYear() - props.birthDate.getFullYear()
   return (
-    <div className='greeting'>{`My name is ${props.firstName} ${props.lastName}. I'm ${age} years old`}</div>
+    <div className='greeting'>{`My name is ${props.firstName} ${props.lastName}. I am ${age} years old`}</div>
   )
 }
 
